b19e575ef44a9bcbf9619c16b3132dc5dcfc3e7f
Pro_Dev <developerteam888@outlook.com>
2020-09-14 08:48:21 +0800

    last

--- data.js
+++ data.js
@@ -1,49 +1,45 @@
 let data = '{ \
 "data":[ \
-	{"title":"Toesox(Socks Shop Site)", \
-	"comment":"toesox.com is a E Commerce Website.", \
+	{"title":"Sleep App", \
+	"comment":"Sleep app is android app.", \
 	"responsible":"Front-end, partial back-end", \
-	"skills":"magento", \
-	"imgs":[ "img/works/toesox/1.jpg", \
-			"img/works/toesox/2.jpg", \
-			"img/works/toesox/3.jpg"] \
+	"skills":"Android, Kotlin, Firebase, RealmDB, Retrofit", \
+	"imgs":[ "img/works/sleep/1.jpg", \
+			"img/works/sleep/2.jpg", \
+			"img/works/sleep/3.jpg"] \
 	}, \
-	{"title":"D U S K(Luxury Bedding Shop Site)", \
-	"comment":"dusk.com is a E Commerce Website.", \
+	{"title":"Dream App", \
+	"comment":"Dream App is android app.", \
 	"responsible":"Front-end, partial back-end", \
-	"skills":"magento", \
-	"imgs":[ "img/works/DUSK/1.jpg", \
-			"img/works/DUSK/2.jpg", \
-			"img/works/DUSK/3.jpg"] \
+	"skills":"Android, Kotlin, Firebase, RealmDB, Retrofit", \
+	"imgs":[ "img/works/dreame/1.jpg", \
+			"img/works/dreame/2.jpg", \
+			"img/works/dreame/1.jpg"] \
 	}, \
-	{"title":"a ★ space(Children’s Furniture Shop Site)", \
-	"comment":"aspace.co.uk is a E Commerce Website.", \
+	{"title":"JW-CAD View App", \
+	"comment":"JW-CAD View is mobile app for view of JW-CAD files.", \
 	"responsible":"Front-end, partial back-end", \
-	"skills":"magento", \
-	"imgs":[ "img/works/aspace/1.jpg", \
-			"img/works/aspace/2.jpg", \
-			"img/works/aspace/3.jpg"] \
+	"skills":"Android, Kotlin, OpenCV, Firebase, RealmDB, Retrofit", \
+	"imgs":[ "img/works/jwcad/1.jpg", \
+			"img/works/jwcad/2.jpg", \
+			"img/works/jwcad/3.jpg"] \
 	}, \
-	{"title":"Stars Assessment", \
-	"comment":"A service which helps employers or companies to evaluates employee’s personolity and talent.", \
+	{"title":"Shooting Game App", \
+	"comment":"This is FPS First person offline shooting game", \
 	"responsible":"Front-end, partial back-end", \
-	"skills":"Angular, Bootstrap, PHP, PDF & Excel, MySQL", \
-	"imgs":[ "img/works/assesment/1.jpg", \
-			"img/works/assesment/2.jpg", \
-			"img/works/assesment/3.jpg", \
-			"img/works/assesment/4.jpg", \
-			"img/works/assesment/5.jpg", \
-			"img/works/assesment/6.jpg", \
-			"img/works/assesment/7.jpg", \
-			"img/works/assesment/8.jpg"] \
+	"skills":"Unity, Android, Java, RealmDB ", \
+	"imgs":[ "img/works/shooting/1.jpg", \
+			"img/works/shooting/2.jpg", \
+			"img/works/shooting/3.jpg", \
+			"img/works/shooting/4.jpg", \
+			"img/works/shooting/5.jpg"] \
 	}, \
-	{"title":"Thread Forum Tool", \
-	"comment":"online forum site that enables users to open a forum of constant subject and	to submit their opinion.", \
+	{"title":"Loans App", \
+	"comment":"online free service for loans", \
 	"responsible":"Front-end, partial back-end", \
-	"skills":"Angular, Bootstrap, PHP, MySQL", \
-	"imgs":[ "img/works/threadbbs/1.jpg", \
-			"img/works/threadbbs/2.jpg", \
-			"img/works/threadbbs/3.jpg"] \
+	"skills":"iOS, Swift, UIKit, RealmDB, Alamofire, Angular", \
+	"imgs":[ "img/works/soccer/1.jpg", \
+			"img/works/soccer/2.jpg"] \
 	}, \
 	{"title":"Hand Crowd", \
 	"comment":"A service that helps project managers. Project managers can create projects, split it in tasks or missions, and assign them to employees. The system shows status of the project	visually and helps the manager to find proper employees for a specific task based on their skills and their idle state.", \
